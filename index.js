c0fda9e9373790ca1e9c99da2cd5f383ed3f84eb
Johnny <suiyi8760@163.com>
2017-12-20 16:57:27 +0800

    finish

---
+++ index.js
@@ -0,0 +1,73 @@
+#!/usr/local/bin/node
+
+console.log("hello-cli")
+
+const fs = require('fs')
+const path = require('path')
+
+const PATH = "."
+const templatePath = path.join(__dirname, 'template')
+let config = {}
+
+/*process.argv.slice(2).forEach(item => {
+    switch (item) {
+        case "-j":
+            config.jquery = true;
+            break;
+        case "-s":
+            config.swiper = true;
+            break;
+        case "-v":
+            config.vue = true;
+            break;
+        case "-b":
+            config.bootstrap = true;
+            break;
+    }
+});*/
+
+function copyTemplate(from, to) {
+    // from = path.join(templatePath, from)
+    write(to, fs.readFileSync(from, 'utf-8'))
+}
+
+function write(path, str, mode) {
+    // console.log(path);
+    fs.writeFileSync(path, str)
+}
+
+function mkdir(path, fn) {
+    fs.mkdir(path, err => {
+        if (err) throw err
+        fn && fn()
+    })
+}
+
+function readdir(path, fn) {
+    // console.log(path);
+    fs.readdir(path, (err, files) => {
+        if (err) throw err
+        fn && fn(files)
+    })
+}
+
+const pipeFile = (from, to) => readdir(from, files => {
+    files.filter(fileName => fileName !== '.DS_Store')
+        .forEach(fileName => {
+            console.log(path.join(to, fileName));
+            fs.stat(path.join(from, fileName), (err, stats) => {
+                if (err) throw err
+                if (stats.isFile()) {
+                    copyTemplate(path.join(from, fileName), path.join(to, fileName))
+                } else if (stats.isDirectory()) {
+                    mkdir(path.join(to, fileName), () => pipeFile(path.join(from, fileName), path.join(to, fileName)))
+                }
+            })
+        })
+})
+
+pipeFile(templatePath, PATH)
+
+/*
+console.log("build complete.");
+console.log(" use \"npm run sass\" to watch the sass file changes.");*/
